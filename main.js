19517817d273d0956ceb065a8411bf017516a689
shunji-work <syung1204@gmail.com>
2026-07-01 11:30:00 +0900

    feat: add todo state rendering milestone

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-const btn = document.querySelector('#add-btn')
-const input = document.querySelector('#todo-input')
-const ul = document.querySelector('#todo-list')
-btn.addEventListener('click',() => {
-const li = document.createElement('li')
-li.textContent = input.value
-ul.appendChild(li)
-input.value = ""
+const btn = document.querySelector('#add-btn');
+const input = document.querySelector('#todo-input');
+const ul = document.querySelector('#todo-list');
+let todos = [];
+
+btn.addEventListener('click', () => {
+  const text = input.value;
+  const id = Date.now();
+  todos.push({ id, text });
+  render();
+  input.value = "";
 });
 
-console.log(btn);
+function render() {
+  ul.innerHTML = "";
+
+  todos.forEach((todo) => {
+    const li = document.createElement('li');
+    li.textContent = todo.text;
+    ul.appendChild(li);
+  });
+}
